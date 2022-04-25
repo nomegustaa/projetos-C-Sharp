bc150181796add004419a2622691f7338d05c173
Gustavo Carvalho da Silva <gustavo.silva@nm.local>
2022-04-25 11:56:21 -0300

    Par Ou Impar

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ParOuImpar{
+    
+    class Program{
+        static void Main(string[]args){
+
+            Console.Clear();
+            Console.WriteLine("Descubra se o número é par ou impar");
+            Console.WriteLine("");
+            Console.WriteLine("Digite um número: ");
+            int num = int.Parse(Console.ReadLine());
+            int soma = num%2;
+            if(soma == 0){
+                Console.WriteLine($"O número {num} é PAR");
+            }else{
+                Console.WriteLine($"O número {num} é ÍMPAR");
+            }
+        }
+    }
+}
